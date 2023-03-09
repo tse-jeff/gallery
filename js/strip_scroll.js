cde5c55c7a49238fc07282f97db26e1dcd899e20
tse-jeff <jeff.tse@nyu.edu>
2023-03-09 14:51:03 -0500

    added images and displayed it temporarily

--- js/strip_scroll.js
+++ js/strip_scroll.js
@@ -1,6 +1,33 @@
 // javascript to generate the scrollable strip of images
 // for now use kittens, but later use the images from the
-// resources directory
+// resources\pictures directory
+pictures = []
+// var fs = require('fs');
+
+// function readFiles(dirname, onFileContent, onError) {
+//   fs.readdir(dirname, function(err, filenames) {
+//     if (err) {
+//       onError(err);
+//       return;
+//     }
+//     filenames.forEach(function(filename) {
+//       fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+//         if (err) {
+//           onError(err);
+//           return;
+//         }
+//         onFileContent(filename, content);
+//       });
+//     });
+//   });
+// }
+
+// var data = {};
+// readFiles('dirname/', function(filename, content) {
+//   data[filename] = content;
+// }, function(err) {
+//   throw err;
+// });
 
 // create the scrollable strip of images
 function createStrip() {
@@ -12,8 +39,12 @@ function createStrip() {
         img.alt = "kitten" + i;
         img.className = "strip_image";
         img.onclick = function() {
-            changeImage(this);
+            // enlarge image while blurring the rest of the page
+            // and display the image name
+
         };
         strip.appendChild(img);
     }
 }
+
+window.onload = createStrip;
