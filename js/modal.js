05291c37f93ea5d598da531dd14858e568c0d3bc
tse-jeff <jeff.tse@nyu.edu>
2023-05-02 19:44:29 -0400

    working modal for pictures

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  
+  // When an image is clicked, set the lightbox image source and show the lightbox
+  $('img[data-bs-toggle="modal"]').click(function() {
+    var imageSrc = $(this).attr('src');
+    $('#lightbox-image').attr('src', imageSrc);
+    $('#lightbox').modal('show');
+  });
+});
